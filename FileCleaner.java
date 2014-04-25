ec5d46b11cb071bcdd2066cd77ec2607a23703e1
jimthenerd <jimthenerd@hotmail.com>
2014-04-24 22:30:41 -0400

    Added Main Functionality

--- FileCleaner.java
+++ FileCleaner.java
@@ -22,6 +22,7 @@ public class FileCleaner extends JFrame {
 		
 		
 		final DefaultListModel<String> flist = new DefaultListModel<String>();
+		final ArrayList<File> fileList = new ArrayList<File>();
 		JList files = new JList(flist);
 		files.setBounds(10, 10, 600, 550);
 		this.add(files);
@@ -32,7 +33,8 @@ public class FileCleaner extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				
+				flist.clear();
+				fileList.clear();
 				JFileChooser chooser = new JFileChooser();
 				
 				chooser.setDialogTitle("Select Folder");
@@ -47,13 +49,54 @@ public class FileCleaner extends JFrame {
 				ArrayList<File> files = new ArrayList<File>();
 				queryFiles(files, path);
 				
+				ProgressWindow w = new ProgressWindow();
+				
 				for (int i=0; i<files.size(); i++){
 					flist.addElement(files.get(i).getAbsolutePath());
+					System.out.println(files.get(i).getAbsolutePath());
+					
+					fileList.add(files.get(i));
+					w.countUp();
 				}
 				
+				w.close();
+				
 			}
+			
+			
+			
 		});
 		this.add(scan);
+		
+		JButton report = new JButton("Generate Report");
+		report.setBounds(620, 50, 160, 30);
+		report.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				final ArrayList<File> firstGroup = new ArrayList<File>();
+				final ArrayList<File> secondGroup = new ArrayList<File>();
+				
+				String report = "Duplicate Files: \n\n";
+				
+				for (int i=0; i<flist.size(); i++){
+					for (int j=0; j<i; j++){
+						if (fileList.get(i).getName().equals(fileList.get(j).getName())){
+							firstGroup.add(fileList.get(i));
+							secondGroup.add(fileList.get(j));
+							report += (fileList.get(i).getAbsolutePath() + "\n" +
+									   fileList.get(j).getAbsolutePath() + "\n\n");
+						}
+					}
+				}
+				
+				report += "\n" + "End of Report";
+				
+				new ReportViewer(report, "Duplicate Files");
+			}
+			
+		});
+		this.add(report);
+		
 		
 		
 		this.repaint();
